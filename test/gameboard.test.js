14a0aa3f030cc7c1dbb2303a2bacccb5b9be4084
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2024-09-08 12:06:48 +0300

    refactor: Refactor tests

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,44 +1,46 @@
 import Gameboard from '../src/gameboard';
 import Ship from '../src/ship';
 
-test('place ship at coordinate', () => {
-  const board = new Gameboard();
-  board.addShip(4, 4, 1);
-  expect(board.getCell(4, 4).ship).toBeInstanceOf(Ship);
-});
+describe('Gameboard', () => {
+  let board;
 
-test('place ship at vertical orientation', () => {
-  const board = new Gameboard();
-  board.addShip(4, 4, 4, true);
-  expect(board.getCell(7, 4).ship).toBeInstanceOf(Ship);
-});
+  beforeEach(() => {
+    board = new Gameboard();
+  });
 
-test('game board receives an attack that hits', () => {
-  const board = new Gameboard();
-  board.addShip(4, 4, 4);
-  expect(board.receiveAttack(4, 6)).toStrictEqual(true);
-  expect(board.getCell(4, 4).ship.hits).toStrictEqual(1);
-  expect(board.getCell(4, 6).isShot).toStrictEqual(true);
-});
+  test('place ship at coordinate', () => {
+    board.addShip(4, 4, 1);
+    expect(board.getCell(4, 4).ship).toBeInstanceOf(Ship);
+  });
 
-test('game board receives an attack that misses', () => {
-  const board = new Gameboard();
-  expect(board.receiveAttack(4, 6)).toStrictEqual(true);
-  expect(board.getCell(4, 6).isShot).toStrictEqual(true);
-  expect(board.getCell(4, 4).ship).toBeNull();
-});
+  test('place ship at vertical orientation', () => {
+    board.addShip(4, 4, 4, true);
+    expect(board.getCell(7, 4).ship).toBeInstanceOf(Ship);
+  });
 
-test('game board denies attacking an attacked cell', () => {
-  const board = new Gameboard();
-  expect(board.receiveAttack(4, 6)).toStrictEqual(true);
-  expect(board.receiveAttack(4, 6)).toStrictEqual(false);
-});
+  test('game board receives an attack that hits', () => {
+    board.addShip(4, 4, 4);
+    expect(board.receiveAttack(4, 6)).toStrictEqual(true);
+    expect(board.getCell(4, 4).ship.hits).toStrictEqual(1);
+    expect(board.getCell(4, 6).isShot).toStrictEqual(true);
+  });
 
-test('game board checks for gameover', () => {
-  const board = new Gameboard();
-  board.addShip(5, 5, 2);
-  expect(board.isGameOver()).toStrictEqual(false);
-  board.receiveAttack(5, 5);
-  board.receiveAttack(5, 6);
-  expect(board.isGameOver()).toStrictEqual(true);
+  test('game board receives an attack that misses', () => {
+    expect(board.receiveAttack(4, 6)).toStrictEqual(true);
+    expect(board.getCell(4, 6).isShot).toStrictEqual(true);
+    expect(board.getCell(4, 4).ship).toBeNull();
+  });
+
+  test('game board denies attacking an attacked cell', () => {
+    expect(board.receiveAttack(4, 6)).toStrictEqual(true);
+    expect(board.receiveAttack(4, 6)).toStrictEqual(false);
+  });
+
+  test('game board checks for gameover', () => {
+    board.addShip(5, 5, 2);
+    expect(board.isGameOver()).toStrictEqual(false);
+    board.receiveAttack(5, 5);
+    board.receiveAttack(5, 6);
+    expect(board.isGameOver()).toStrictEqual(true);
+  });
 });
--- test/ship.test.js
+++ test/ship.test.js
@@ -1,14 +1,20 @@
 import Ship from '../src/ship';
 
-test('Increase hits when ship is hit.', () => {
-  const ship = new Ship(1);
-  const hits = ship.hits;
-  ship.hit();
-  expect(ship.hits).toBe(hits + 1);
-});
+describe('Ship', () => {
+  let ship;
 
-test('calculate if ship is sunk', () => {
-  const ship = new Ship(1);
-  ship.hit();
-  expect(ship.isSunk()).toBe(true);
+  beforeEach(() => {
+    ship = new Ship(1);
+  });
+
+  test('Increase hits when ship is hit.', () => {
+    const hits = ship.hits;
+    ship.hit();
+    expect(ship.hits).toBe(hits + 1);
+  });
+
+  test('calculate if ship is sunk', () => {
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
 });
